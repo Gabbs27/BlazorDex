edc11d67be6e0ef0cc299f3e6742d5532b469f58
Gabriel <djabreu_@hotmail.com>
2024-11-18 22:36:19 -0400

    Enhanced Service and UI

--- BlazorDex/Services/PokeService.cs
+++ BlazorDex/Services/PokeService.cs
@@ -32,10 +32,44 @@ public class Pokemon
     public int Weight { get; set; }
     public int Height { get; set; }
     public Sprites? Sprites { get; set; }
-    
+    public List<TypeEntry>? Types { get; set; }
+    public List<AbilityEntry>? Abilities { get; set; }
+    public List<StatEntry>? Stats { get; set; }
 }
 
 public class Sprites
 {
     public string? Front_Default { get; set; }
+    public string? Back_Default { get; set; }
+}
+
+public class TypeEntry
+{
+    public TypeInfo? Type { get; set; }
+}
+
+public class TypeInfo
+{
+    public string? Name { get; set; }
+}
+
+public class AbilityEntry
+{
+    public AbilityInfo? Ability { get; set; }
+}
+
+public class AbilityInfo
+{
+    public string? Name { get; set; }
+}
+
+public class StatEntry
+{
+    public StatInfo? Stat { get; set; }
+    public int Base_Stat { get; set; }
+}
+
+public class StatInfo
+{
+    public string? Name { get; set; }
 }
